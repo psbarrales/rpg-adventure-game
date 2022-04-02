4301f0bbbb9af955b340040a9f2130efdcc4b5b1
Pablo Barrales <pablo.barrales@walmart.com>
2022-04-01 23:41:23 -0300

    Initial project

---
+++ core/src/main/java/psbarrales/game/rpg/MainGame.java
@@ -0,0 +1,11 @@
+package psbarrales.game.rpg;
+
+import com.badlogic.gdx.Game;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class MainGame extends Game {
+	@Override
+	public void create() {
+		setScreen(new FirstScreen());
+	}
+}
